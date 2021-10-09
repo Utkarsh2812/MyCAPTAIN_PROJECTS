49e5510e71edd751d8261741ec0844e65982dda9
Utkarsh Rastogi <91966216+Utkarsh2812@users.noreply.github.com>
2021-10-09 08:48:15 +0530

    Add files via upload

---
+++ Project1_area_of-circle.py
@@ -0,0 +1,8 @@
+import math
+
+r = float(input('Input the radius of the circle: '))
+
+pie = math.pi
+area = pie * (r * r)
+
+print(f'The area of the circle with radius {r} is {area}')
---
+++ Project1_file_extension.py
@@ -0,0 +1,4 @@
+filename = input('Input the file name: ')
+
+if(filename.endswith('.py')):
+    print('The extension of that file is Python')
