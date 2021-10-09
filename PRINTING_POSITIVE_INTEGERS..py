cfb5635d3173fb05e0ebf476d6fd86f4422fba71
Utkarsh Rastogi <91966216+Utkarsh2812@users.noreply.github.com>
2021-10-09 14:36:12 +0530

    Add files via upload

---
+++ PRINTING_POSITIVE_INTEGERS..py
@@ -0,0 +1,17 @@
+
+
+list1 = [12 , -7 , 5 , 64 , -14]
+list2 = [12, 14, -95, 3]
+Output1 = []
+Output2 = []
+
+for num in list1:
+    if num > 0:
+        Output1.append(num)
+
+print("Output 1: " , Output1)
+for numb in list2:
+    if numb > 0:
+        Output2.append(numb)
+
+print("Output 2: " , Output2)
