6b66b73c71d4237d4c8584de967b321b43ed2153
Utkarsh Rastogi <91966216+Utkarsh2812@users.noreply.github.com>
2021-10-21 11:21:50 +0530

    Update fun_most_frequent.py

--- fun_most_frequent.py
+++ fun_most_frequent.py
@@ -1,12 +1,8 @@
-
-def most_frequent(sentence):
-    a = s.count("i")
-    b = s.count("s")
-    c = s.count("p")
-    d = s.count("M")
-    Dict = {"M":d , "i":a, 's':b, 'p':c}
-    print(f'''i = 0{(Dict["i"])}, s = 0{(Dict["s"])}, p = 0{(Dict['p'])}, m = 0{(Dict['M'])}''' )
-
-s = input("Please Enter a String ")
-most_frequent(s)
-
+str1 = input("Enter a String: ")
+  
+res = {}
+  
+for keys in str1:
+    res[keys] = res.get(keys, 0) + 1
+  
+print (str(res))
